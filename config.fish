77d6639cae07d5e7b584a408a6483b6d58a0837d
Andy Dirnberger <dirn@dirnonline.com>
2015-09-25 17:35:11 -0400

    Enable virtualfish plugins

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-eval (python -m virtualfish)
+eval (python -m virtualfish auto_activation global_requirements projects)
 
 set -x __fish_git_prompt_showdirtystate true
 set -x ___fish_git_prompt_color_branch (set_color blue)
