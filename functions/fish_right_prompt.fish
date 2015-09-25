41de82f19014fa7dd568851ba011862b466b9bdd
Andy Dirnberger <dirn@dirnonline.com>
2015-09-25 17:34:31 -0400

    Add battery percentage to the prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,4 +1,6 @@
 function fish_right_prompt --description 'Write out the right prompt'
+    set -l columns (tput cols)
+
     set -l code $status
     if test $code != 0
         set_color --bold red
@@ -6,43 +8,63 @@ function fish_right_prompt --description 'Write out the right prompt'
         set_color normal
     end
 
-    if set -q __fish_vi_mode
-        set -l columns (tput cols)
+    set -l battery (pmset -g batt | egrep "([0-9]+\%).*" -o | cut -f1 -d"%")
+    if test $battery -lt 40
+        set __fish_battery_color white
+        set __fish_battery_background red
+    else
+        set __fish_battery_color white
+        set __fish_battery_background blue
+    end
 
+    set_color --bold $__fish_battery_background --background normal
+    echo ""
+    set_color --bold $__fish_battery_color --background $__fish_battery_background
+    if test $columns -lt 120
+        echo "$battery%"
+    else
+        echo " $battery%"
+    end
+
+    if set -q __fish_vi_mode
         if test -z $__fish_mode_prompt_insert
             if test $columns -lt 120
                 set __fish_mode_prompt_insert '[I]'
             else
-                set __fish_mode_prompt_insert '[INSERT]'
+                set __fish_mode_prompt_insert ' [INSERT] '
             end
         end
         if test -z $__fish_mode_prompt_normal
             if test $columns -lt 120
                 set __fish_mode_prompt_normal '[N]'
             else
-                set __fish_mode_prompt_normal '[NORMAL]'
+                set __fish_mode_prompt_normal ' [NORMAL] '
             end
         end
         if test -z $__fish_mode_prompt_visual
             if test $columns -lt 120
                 set __fish_mode_prompt_visual '[V]'
             else
-                set __fish_mode_prompt_visual '[VISUAL]'
+                set __fish_mode_prompt_visual ' [VISUAL] '
             end
         end
 
         echo ' '
         switch $fish_bind_mode
             case default
-                set_color --bold --background red white
-                echo "$__fish_mode_prompt_normal"
+                set __fish_mode_background red
+                set __fish_mode_prompt_text $__fish_mode_prompt_normal
             case insert
-                set_color --bold --background green white
-                echo "$__fish_mode_prompt_insert"
+                set __fish_mode_background yellow
+                set __fish_mode_prompt_text $__fish_mode_prompt_insert
             case visual
-                set_color --bold --background magenta white
-                echo "$__fish_mode_prompt_visual"
+                set __fish_mode_background magenta
+                set __fish_mode_prompt_text $__fish_mode_prompt_visual
         end
+        set_color --bold $__fish_mode_background --background $__fish_battery_background
+        echo ""
+        set_color --bold white --background $__fish_mode_background
+        echo "$__fish_mode_prompt_text"
         set_color normal
     end
 end
