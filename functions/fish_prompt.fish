c9f6f9185eb6a4832e0bc1c2d2e19fda928d3bd6
Andy Dirnberger <dirn@dirnonline.com>
2015-09-29 15:17:11 -0400

    echo, don't print

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,31 +3,30 @@ function fish_prompt
 
     # Virtual environment
     if test $VIRTUAL_ENV
-        printf "(%s) " (set_color red)(basename $VIRTUAL_ENV)(set_color normal)
-        echo
+        echo "("(set_color red)(basename $VIRTUAL_ENV)(set_color normal)")"
     end
 
     # Line 1
 
     # User
     set_color yellow
-    printf '%s' (whoami)
+    echo -n (whoami)
     set_color normal
 
     # Host
-    printf ' at '
+    echo -n " at "
     set_color magenta
-    printf '%s' (hostname|cut -d . -f 1)
+    echo -n (hostname | cut -f1 -d".")
     set_color normal
 
     # Current working directory
-    printf ' in '
+    echo -n " in "
     set_color $fish_color_cwd
-    printf '%s' (pwd|sed "s=$HOME=~=")
+    echo -n (pwd | sed "s=$HOME=~=")
     set_color normal
 
     # Git
-    printf '%s' (__fish_git_prompt)
+    echo -n (__fish_git_prompt)
     set_color normal
 
     # Line 2
